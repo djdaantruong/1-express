4817751fcdfaebb4a1d1a512cc8939ce8da11801
djdaantruong <daantd.a12017@gmail.com>
2020-07-23 00:06:26 +0700

    bai 14 cookie

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require("body-parser");
+var cookieParser = require('cookie-parser');
 var userRoute = require('./routes/user.route');
 
 var port = 3000;
@@ -10,6 +11,7 @@ app.set('views', './views');
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(cookieParser());
 
 app.use(express.static('public'));
 
