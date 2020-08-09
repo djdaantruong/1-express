770a24b5a33af1d930f9bb6be862f2fcec2bc480
djdaantruong <daantd.a12017@gmail.com>
2020-08-09 14:48:37 +0700

    bai 18 environment variable

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+console.log(process.env.SESSION_SECRET);
+
 var express = require('express');
 var bodyParser = require("body-parser");
 var cookieParser = require('cookie-parser');
@@ -15,7 +18,7 @@ app.set('views', './views');
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.use(cookieParser('atasghjhdkas7428'));
+app.use(cookieParser(process.env.SESSION_SECRET));
 
 app.use(express.static('public'));
 
