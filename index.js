42b904a94cde58064f01e50f18323c73a572802b
djdaantruong <daantd.a12017@gmail.com>
2020-08-11 02:03:25 +0700

    bai 20 pagination

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-console.log(process.env.SESSION_SECRET);
 
 var express = require('express');
 var bodyParser = require("body-parser");
@@ -7,6 +6,7 @@ var cookieParser = require('cookie-parser');
 
 var userRoute = require('./routes/user.route');
 var authRoute = require('./routes/auth.route');
+var productRoute = require('./routes/product.route');
 
 var authMiddleware = require('./middlewares/auth.middleware');
 
@@ -30,6 +30,7 @@ app.get('/', function(req, res){
 
 app.use('/users', authMiddleware.requireAuth, userRoute);
 app.use('/auth', authRoute);
+app.use('/products', productRoute);
 
 app.listen(port, function(){
 	console.log('Server listening on port' + port);
