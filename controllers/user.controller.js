b01e738743ddf2b101fc322f500b4a15844e0585
djdaantruong <daantd.a12017@gmail.com>
2020-08-11 15:43:16 +0700

    bai 21 file uploads

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -43,7 +43,8 @@ module.exports.getDelete = function(req, res) {
 
 
 module.exports.postCreate = function(req, res) {
-	req.body.id = shortid.generate();
+  req.body.id = shortid.generate();
+  req.body.avatar = req.file.path.split('/').slice(1).join('/');
   db.get('users').push(req.body).write();
   res.redirect('/users');
  };
